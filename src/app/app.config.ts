5ad3be92b0aa581fbe95878cc66a19e3cbcf3a18
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2025-03-14 21:45:30 -0600

    Add ngx-translate support for internationalization and configure HTTP loader

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import {SsrCookieService} from 'ngx-cookie-service-ssr';
 
@@ -8,11 +8,35 @@ import {
   withEventReplay,
 } from '@angular/platform-browser';
 
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    provideHttpClient(
+      withFetch(),
+    ),
+
+    // Add your providers here for the server side
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+      }),
+    ),
+
     SsrCookieService,
   ],
 };
